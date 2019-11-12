27e3511e9ce1338db03a99ab383d5d80ae42a889
jean <jpaltahonacc@gmail.com>
2019-11-12 14:12:54 -0500

    add loading

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -6,30 +6,39 @@ import * as usuariosTypes from '../types/usuariosTypes';
 const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
 
-    const { usuarios } = getState().usuariosReducers;
-    const { publicaciones } =getState().publicacionesReducer
-    const usuario_id = usuarios[key].id;
 
-    const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`)
+    try{
+        const { usuarios } = getState().usuariosReducers;
+        const { publicaciones } =getState().publicacionesReducer
+        const usuario_id = usuarios[key].id;
+        const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`)
 
-    const publicaciones_actualizadas = [
-        ...publicaciones,
-        respuesta.data
-    ];
-
-    const publicaciones_key = publicaciones_actualizadas.length -1;
-    const usuarios_actualizados = [...usuarios];
-    usuarios_actualizados[key] = {
-        ...usuarios[key],
-        publicaciones_key
-    }
+        const publicaciones_actualizadas = [
+            ...publicaciones,
+            respuesta.data
+        ];
     
-    dispatch({
-        type: USUARIOS_TRAER_TODOS,
-        payload: usuarios_actualizados
-    })
-    dispatch({
-        type: TRAER_POR_USUARIO,
-        payload: publicaciones_actualizadas
-    })
+        const publicaciones_key = publicaciones_actualizadas.length -1;
+        const usuarios_actualizados = [...usuarios];
+        usuarios_actualizados[key] = {
+            ...usuarios[key],
+            publicaciones_key
+        }
+        
+        dispatch({
+            type: USUARIOS_TRAER_TODOS,
+            payload: usuarios_actualizados
+        })
+        dispatch({
+            type: TRAER_POR_USUARIO,
+            payload: publicaciones_actualizadas
+        })
+    }
+    catch(error){
+        dispatch({
+            type: ERROR,
+            payload: 'error'
+        })
+    }
+
 }
--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -2,7 +2,8 @@ import React ,{ Component } from 'react';
 import {connect} from 'react-redux';
 import * as usuariosActions from '../../actions/usuariosActions';
 import * as publicacionesAction from '../../actions/publicacionesActions';
-
+import Spinner from '../general/Spinner';
+import Fatal from '../general/Fatal';
 //Lista de acciones
 const { traerTodos: usuariosTraerTodos } = usuariosActions;
 const { traerPorUsuario: PublicacionestraerPorUsuario } = publicacionesAction; 
@@ -12,10 +13,42 @@ const { traerPorUsuario: PublicacionestraerPorUsuario } = publicacionesAction;
 class Publicaciones extends Component{
  
    async componentDidMount(){
+       const {
+        usuariosTraerTodos,
+        PublicacionestraerPorUsuario,
+        match: { params: { key } }
+       } = this.props;
+
         if(!this.props.usuariosReducers.usuarios.length){
-           await this.props.usuariosTraerTodos();
+           await usuariosTraerTodos();
         }
-        this.props. PublicacionestraerPorUsuario( this.props.match.params.key );
+        if(this.props.usuariosReducers.error != ''){
+            return;
+        }
+        if(!('publicaciones_key' in this.props.usuariosReducers.usuarios[ key ])){
+            PublicacionestraerPorUsuario( key );
+        }
+    }
+
+    ponerUsuario = () => {
+        const {
+            usuariosReducers,
+            match: { params: { key } }
+        } = this.props;
+
+        
+        if(!usuariosReducers.usuarios.length || usuariosReducers.cargando){
+            return <Spinner />
+        }
+        if(this.props.publicacionesReducer.error){
+            return <Fatal error={'algo salio mal'} />
+        }
+        const nombre = usuariosReducers.usuarios[key].name;
+        return (
+            <h1>
+                publicaciones de {nombre}
+            </h1>
+        )
     }
 
     render(){
@@ -24,6 +57,7 @@ class Publicaciones extends Component{
             <div>
                 <h1>Publicaciones De:</h1>
                {this.props.match.params.key}
+               {this.ponerUsuario()}
             </div>
         )
     }
